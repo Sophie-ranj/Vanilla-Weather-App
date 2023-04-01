385a0c558e1bf9a7cadbed6d9a843c769c36df50
safoora1986 <safoora.ranjbaran@gmail.com>
2023-04-01 13:45:47 +0100

    API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-// alert("Byyyye");
+function dis(reponse) {
+  console.log("111111", reponse.data.city);
+  console.log("222222", reponse.data.country);
+}
+
+let apiKey = "382f83908a2d98boe61baf6df768d4tb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=382f83908a2d98boe61baf6df768d4tb&units=metric`;
+
+axios.get(apiUrl).then(dis);
